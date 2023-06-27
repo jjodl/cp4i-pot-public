275cd72db0d7428c093e0fd0de9ba70e493dec98
JOSEPH JODL <jjodl@us.ibm.com>
2023-06-27 13:46:44 -0500

    cleanup

--- Event_EndPoint_save/Lab_1/lab_setup/deployApis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCRIPT=_deployApis.sh
-BASEPATH=$(x=$(realpath "${0}") && dirname "${x}")
-
-docker run --mount type=bind,source="${BASEPATH}",target="/tmp/mounted" \
---mount type=bind,source="$(echo ~/.kube/config)",target="/root/.kube/config" \
-openshift/origin-cli sh /tmp/mounted/"${SCRIPT}"
--- Event_EndPoint_save/Lab_2/ReadMe.md
+++
@@ -1,7 +0,0 @@
-## Multi-Style Integration with IBM Cloud Pak for Integration Event Endpoint Experience
-
-[Return to main lab page](../index.md)
-
----
-
-## This is the Event EndPoint lab and is being built. 
